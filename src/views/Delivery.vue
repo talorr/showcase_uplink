96dffec1e95fbccd0658fd81804403beed233b65
talorr <talorr31@gmail.com>
2024-08-30 12:12:37 +0700

    fix

--- src/views/Delivery.vue
+++ src/views/Delivery.vue
@@ -2,8 +2,32 @@
   <MainHeader />
 
   <div class="header-margin container info">
-    <h1> {{ data.pagetitle }} </h1>
-    <div v-html="data.content"></div>
+    <h1> Условия доставки </h1>
+    <div>
+      <p><span >Доставка цветов осуществляется в городе </span>{{ siteInfoStore.siteInfo.city }}<span >.</span></p>
+      <p><span >Стоимость доставки:</span></p>
+      <ul>
+        <li v-for="delivery in deliveriesList" :key="delivery.id" >
+          {{ delivery.description === '0' ? "Самовывоз " : "Доставка "}}{{ delivery.name }}<span > - </span>{{ delivery.price }} <span >₽;</span>
+        </li>
+      </ul>
+      <p><span >Если у вас остались вопросы, свяжитесь с нашими менеджерами по телефону </span>
+        <a :href="'tel:' + phoneToCall">
+        {{ siteInfoStore.siteInfo.phone }}
+        </a>
+        .
+      </p>
+
+      <h2>Дополнительная информация</h2>
+      <p><span >Укажите дополнительную информацию при необходимости:</span></p>
+      <ul>
+        <li ><span >Стоимость бесплатной доставки;</span></li>
+        <li ><span >Сроки доставки;</span></li>
+        <li ><span >Информация об отдаленных районах доставки;</span></li>
+        <li ><span >Информацию о графике доставки;</span></li>
+        <li ><span >Информацию о доставке в ночное время</span></li>
+      </ul>
+    </div>
   </div>
 
   <Footer/>
@@ -14,11 +38,18 @@ import { useMeta } from "vue-meta";
 useMeta({
   title: "Доставка",
 })
-import { ref, onMounted } from 'vue';
+import { useSiteInfo } from '@/stores/siteInfo';
+const siteInfoStore = useSiteInfo();
+import { ref, onMounted, computed } from 'vue';
 import MainHeader from "../components/MainHeader.vue";
 import Footer from "@/components/Footer.vue";
 import apiClient from "../axios";
-
+async function getDeliveriesList() {
+  let responseDeliveries = await apiClient.get('/deliveries-list');
+  deliveriesList.value = responseDeliveries.data.deliveries
+}
+const phoneToCall = computed(() => siteInfoStore.siteInfo.phone[0] == 8 ? '+7' + siteInfoStore.siteInfo.phone.slice(1) : siteInfoStore.siteInfo.phone);
+let deliveriesList = ref({});
 let data = ref({});
 
 const init = async () => {
@@ -28,5 +59,7 @@ const init = async () => {
 
 onMounted(() => {
   init();
+  getDeliveriesList()
 });
 </script>
+
