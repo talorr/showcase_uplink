b6122a912f8328c314ddb6db1546dabb4dcd13ff
fedorsh <fedor.sh@uplinkweb.ru>
2024-09-10 13:24:56 +0700

    fix

--- src/components/BannerFooter.vue
+++ src/components/BannerFooter.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="footer__banner">
-    <img alt="footer__banner" src="../assets/images/footer__banner.webp" class="footer__banner-left">
-    <img alt="footer__banner" src="../assets/images/footer__banner1.webp" class="footer__banner-right">
+    <img alt="footer__banner" src="@/assets/images/footer__banner.webp" class="footer__banner-left">
+    <img alt="footer__banner" src="@/assets/images/footer__banner1.webp" class="footer__banner-right">
     <a href="https://like.floria.pro" target="_blank" class="footer__banner-text">
       Флория - комплексное продвижение цветочного бизнеса
     </a>
