91aa007e52e70560f597bdf79b35058cd964c4c1
talorr <talorr31@gmail.com>
2024-09-16 12:32:27 +0700

    fix

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -63,16 +63,11 @@
         </div>
       </div>
       <div class="footer__organization">
-        <div>{{ siteInfo.organization_name ? siteInfo.organization_name : "" }}</div>
-        <div>{{ siteInfo.inn ? "ИНН: " + siteInfo.inn : "" }}</div>
-        <div>{{ siteInfo.kpp ? "КПП: " + siteInfo.kpp : "" }}</div>
-        <div>{{ siteInfo.ogrn ? "ОГРН: " + siteInfo.ogrn : "" }}</div>
-        <div>{{ siteInfo.ogrnip ? "ОГРНИП: " + siteInfo.ogrnip : "" }}</div>
-        
-        
-        
-        
-        
+        <div v-show="siteInfo.organization_name">{{ siteInfo.organization_name }}</div>
+        <div v-show="siteInfo.inn">{{ "ИНН: " + siteInfo.inn  }}</div>
+        <div v-show="siteInfo.kpp">{{ "КПП: " + siteInfo.kpp }}</div>
+        <div v-show="siteInfo.ogrn">{{ "ОГРН: " + siteInfo.ogrn }}</div>
+        <div v-show="siteInfo.ogrnip">{{  "ОГРНИП: " + siteInfo.ogrnip}}</div>
       </div>
 
       <BannerFooter/>
